99cd47835470ce793d69eef1698521c71eb2a6c6
Mac Heller-Ogden <mac@machellerogden.com>
2017-07-17 10:12:37 -0500

    major change - no options and no function call anymore just use node -r to require instead

--- README.md
+++ README.md
@@ -10,24 +10,11 @@ npm i --save-dev leak-patrol
 
 ## Usage
 
-Very early as in your application's entrypoint file...
-
 ```
-require('leak-patrol')();
+node -r leak-patrol ./my-app-entrypoint > /dev/null
 ```
-
-And then run your app.
 
 Note: you may want to pipe stdout to /dev/null when you run your app because all logs output from leak-patrol are piped to stderr.
-
-## Options
-
-```
-require('leak-patrol')({
-    snapshotsAtMarkSweep: [ 10, 11 ], // this will take a heap snapshot at marksweep #10 and #11, useful for getting deltas
-    graphDeltas: true // this will print a small graph of values showing the heapTotal delta between mark sweep events
-});
-```
 
 ## License
 
--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const _ = require('lodash');
 const Table = require('easy-table');
 const bytes = require('bytes');
 const profiler = require('gc-profiler');
-const verticalMeter = require('vertical-meter');
 const memHistory = [];
 const normalize = (val, max, min) => ((val - min) / (max - min));
 const v8 = require('v8');
 const heapSizeLimit = v8.getHeapStatistics().heap_size_limit;
 const heapSizeLimitBytes = bytes(heapSizeLimit);
 let markSweepCount = 0;
-let bound = false;
 let openRequests = [];
 let completedRequests = [];
 
@@ -21,6 +19,7 @@ function requestLogger(httpModule){
         const requestStartTime = process.hrtime();
         const reqId = requestStartTime.join('');
         const reqStr = `${options.method} ${options.href || (options.proto + '://' + options.host + options.path)}`;
+        //console.error(reqStr);
         openRequests.push({
             id: reqId,
             info: reqStr
@@ -49,108 +48,80 @@ function requestLogger(httpModule){
     };
 }
 
-module.exports = function leakPatrol(options) {
-    options = options || {};
-    const snapshotsAtMarkSweep = options.snapshotsAtMarkSweep || [];
-    const graphDeltas = options.graphDeltas || false;
+requestLogger(require('http'));
+requestLogger(require('https'));
 
-    if (bound === false) {
-        bound = true;
-        requestLogger(require('http'));
-        requestLogger(require('https'));
-        profiler.on('gc', (info) => {
-            if (info.type === "MarkSweepCompact") {
+profiler.on('gc', (info) => {
+    if (info.type === "MarkSweepCompact") {
 
-                // count mark sweep
-                markSweepCount++;
+        // count mark sweep
+        markSweepCount++;
 
-                const mem = process.memoryUsage();
-                let deltas = {};
-                if (memHistory.length) {
-                    deltas = _.mapKeys(_.assignWith(memHistory[memHistory.length - 1], mem, (lastVal, newVal) => {
-                        return newVal - lastVal;
-                    }), (v, k) => `${k}Delta`);
-                }
-                memHistory.push(_.assign({}, mem));
+        process.emit('lp:marksweep', markSweepCount);
 
-                if (graphDeltas) {
-                    const heapTotalTrend = memHistory.map((entry) => {
-                        return verticalMeter(normalize(entry.heapTotal, heapSizeLimit, -heapSizeLimit));
-                    });
-                    const heapUsedTrend = memHistory.map((entry) => {
-                        return verticalMeter(normalize(entry.heapUsed, heapSizeLimit, -heapSizeLimit));
-                    });
-                    heapTotalTrend.unshift('Heap Total: ');
-                    heapUsedTrend.unshift('Heap Used: ');
-                }
+        const mem = process.memoryUsage();
+        let deltas = {};
+        if (memHistory.length) {
+            deltas = _.mapKeys(_.assignWith(memHistory[memHistory.length - 1], mem, (lastVal, newVal) => {
+                return newVal - lastVal;
+            }), (v, k) => `${k}Delta`);
+        }
+        memHistory.push(_.assign({}, mem));
 
-                const completedRequestsReport = _.reduce(completedRequests, (acc, entry) => {
-                    const i = _.findIndex(acc, { request: entry.info });
-                    if (i !== -1) {
-                        acc[i].count++;
-                        acc[i].averageElapsedTime = (acc[i].averageElapsedTime + entry.elapsed) / 2;
-                    } else {
-                        acc.push({
-                            count: 1,
-                            averageElapsedTime: entry.elapsed,
-                            request: entry.info
-                        });
-                    }
-                    return acc;
-                }, []);
-
-                // clear completed requests
-                completedRequests = [];
-
-                const lastEventLoopTime = process.hrtime();
-                setImmediate(() => {
-                    const eventLoopDelay = process.hrtime(lastEventLoopTime);
-
-                    console.error(`\n*********** ${info.type} #${markSweepCount} ***********`);
-
-                    console.error(Table.print(Object.assign({},
-                        info,
-                        {
-                            pid: process.pid,
-                            heapSizeLimit: heapSizeLimitBytes
-                        },
-                        _.mapValues(mem, bytes),
-                        _.mapValues(deltas, bytes),
-                        {
-                            openRequestCount: openRequests.length,
-                            eventLoopDelay: `${(eventLoopDelay[0] * 1e9 + eventLoopDelay[1]) / 1e6}ms`
-                        }
-                    )));
-
-                    console.error(Table.print(_.map(v8.getHeapSpaceStatistics(), (entry) => {
-                        return _.mapValues(entry, (v) => {
-                            if (_.isNumber(v)) {
-                                return bytes(v);
-                            } else {
-                                return v;
-                            }
-                        });
-                    })));
-
-                    if (graphDeltas) {
-                        console.error.apply(console, heapTotalTrend);
-                        console.error.apply(console, heapUsedTrend);
-                    }
-
-                    if (completedRequests.length) {
-                        console.error('Requests Completed Since Last Mark Sweep:');
-                        console.error(Table.print(completedRequestsReport));
-                    }
-
+        const completedRequestsReport = _.reduce(completedRequests, (acc, entry) => {
+            const i = _.findIndex(acc, { request: entry.info });
+            if (i !== -1) {
+                acc[i].count++;
+                acc[i].averageElapsedTime = (acc[i].averageElapsedTime + entry.elapsed) / 2;
+            } else {
+                acc.push({
+                    count: 1,
+                    averageElapsedTime: entry.elapsed,
+                    request: entry.info
                 });
-
-                if (snapshotsAtMarkSweep.indexOf(markSweepCount) != -1) {
-                    require('heapdump').writeSnapshot((err, filename) => {
-                        if (err) {return console.error(err);}
-                        console.log('dump written to', filename);
-                    });
-                }
             }
+            return acc;
+        }, []);
+
+        // clear completed requests
+        completedRequests = [];
+
+        const lastEventLoopTime = process.hrtime();
+        setImmediate(() => {
+            const eventLoopDelay = process.hrtime(lastEventLoopTime);
+
+            console.error(`\n*********** ${info.type} #${markSweepCount} ***********`);
+
+            console.error(Table.print(Object.assign({},
+                info,
+                {
+                    pid: process.pid,
+                    heapSizeLimit: heapSizeLimitBytes
+                },
+                _.mapValues(mem, bytes),
+                _.mapValues(deltas, bytes),
+                {
+                    openRequestCount: openRequests.length,
+                    eventLoopDelay: `${(eventLoopDelay[0] * 1e9 + eventLoopDelay[1]) / 1e6}ms`
+                }
+            )));
+
+            console.error(Table.print(_.map(v8.getHeapSpaceStatistics(), (entry) => {
+                return _.mapValues(entry, (v) => {
+                    if (_.isNumber(v)) {
+                        return bytes(v);
+                    } else {
+                        return v;
+                    }
+                });
+            })));
+
+            if (completedRequests.length) {
+                console.error('Requests Completed Since Last Mark Sweep:');
+                console.error(Table.print(completedRequestsReport));
+            }
+
         });
+
     }
-}
+});
